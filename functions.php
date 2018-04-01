23f762bcd870611352bf3d68d9cef3f9cbab7697
Nawawish <hydrokidz@gmail.com>
2018-04-01 23:13:32 +0700

    Remove NWP softlink and comment relevant code of NWP.

--- functions.php
+++ functions.php
@@ -7,31 +7,31 @@ autoload( __DIR__ . '/inc/*.php', [
 	'content-callback.php'
 ] );
 
-use NWP\Admin;
-use NWP\Widget;
-use NWP\Menu;
-use NWP\SubMenu;
+// use NWP\Admin;
+// use NWP\Widget;
+// use NWP\Menu;
+// use NWP\SubMenu;
 
-add_action( 'widgets_init', 'arphabet_widgets_init' );
+// add_action( 'widgets_init', 'arphabet_widgets_init' );
 
-/**
- * Functions
- */
+// /**
+//  * Functions
+//  */
 
-/**
- * Register our sidebars and widgetized areas.
- *
- */
-function arphabet_widgets_init() {
+// /**
+//  * Register our sidebars and widgetized areas.
+//  *
+//  */
+// function arphabet_widgets_init() {
 
-	register_sidebar( array(
-		'name'          => 'Home right sidebar',
-		'id'            => 'sidebar-right',
-		'description'   => 'This is side bar description',
-		'before_widget' => '<div>',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h2 class="rounded">',
-		'after_title'   => '</h2>',
-	) );
+// 	register_sidebar( array(
+// 		'name'          => 'Home right sidebar',
+// 		'id'            => 'sidebar-right',
+// 		'description'   => 'This is side bar description',
+// 		'before_widget' => '<div>',
+// 		'after_widget'  => '</div>',
+// 		'before_title'  => '<h2 class="rounded">',
+// 		'after_title'   => '</h2>',
+// 	) );
 
-}
+// }
